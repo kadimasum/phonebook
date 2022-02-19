75624fdcdc27223462f982973d9bd99ae22de3f7
AugustineKadima <kadimasam@gmail.com>
2022-02-19 19:10:05 +0300

    Add setup and setdown methods

--- Contacts_Test.py
+++ Contacts_Test.py
@@ -2,51 +2,47 @@ import unittest
 from Contacts import Contact
 
 class ContactTest(unittest.TestCase):
+    def setUp(self):
+        self.person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
+ 
+    def tearDown(self):
+        Contact.clear_all()
+
     def test_instance_creates_successfully(self):
-        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
-        self.assertTrue(isinstance(person_x, Contact))
+        self.assertTrue(isinstance(self.person_x, Contact))
 
     def test_save_contact(self):
-        Contact.clear_all()
-        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
-        person_x.save()
+        self.person_x.save()
         self.assertEqual(len(Contact.contact_list),1)
-        self.assertEqual(Contact.contact_list[0].first_name, person_x.first_name)
+        self.assertEqual(Contact.contact_list[0].first_name, self.person_x.first_name)
 
     def test_delete_contact(self):
-        Contact.clear_all()
-        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
-        person_x.save()
+        self.person_x.save()
         self.assertEqual(Contact.delete_contact(1), True)
 
     def test_find_contact_by_id(self):
-        Contact.clear_all()
-        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
-        person_x.save()
-        self.assertEqual(person_x.first_name,  Contact.find_contact_by_id(1))
+        self.person_x.save()
+        self.assertEqual(self.person_x.first_name,  Contact.find_contact_by_id(1))
 
     def test_fullname(self):
-        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
-        self.assertEqual(person_x.full_name,"Allan Walker")
+        self.assertEqual(self.person_x.full_name,"Allan Walker")
 
     def test_find_all_contacts(self):
         self.assertEqual(len(Contact.contact_list), len(Contact.find_all_contacts()))
 
 
     def test_update_contact(self):
-        Contact.clear_all()
-        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
-        person_x.save()
+        self.person_x.save()
 
         Contact.update_first_name(1, "Paulo")
         Contact.update_email(1, "paulowalker@gmail.com")
         Contact.update_last_name(1, "Samuel")
         Contact.update_phone_number(1, "06573467")
-        
-        self.assertEqual(person_x.first_name, "Paulo")
-        self.assertEqual(person_x.email, "paulowalker@gmail.com")
-        self.assertEqual(person_x.last_name, "Samuel")
-        self.assertEqual(person_x.phone_number, "06573467")
+
+        self.assertEqual(self.person_x.first_name, "Paulo")
+        self.assertEqual(self.person_x.email, "paulowalker@gmail.com")
+        self.assertEqual(self.person_x.last_name, "Samuel")
+        self.assertEqual(self.person_x.phone_number, "06573467")
 
 
 
