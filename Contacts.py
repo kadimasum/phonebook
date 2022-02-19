7322d9e66b60fbc6a241b554af7bd9e09fa0d792
AugustineKadima <kadimasam@gmail.com>
2022-02-19 17:26:18 +0300

    Add methods to delete, save, find y id, find all, and clear contacts

--- Contacts.py
+++ Contacts.py
@@ -1,6 +1,60 @@
 class Contact:
+
+    contact_list = []
+    id = 0
+
     def __init__(self, first_name, last_name, phone_number, email):
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
+
+    def save(self):
+        if len(Contact.contact_list) == 0:
+            self.id = 1
+            Contact.id  = 1
+            self.contact_list.append(self)
+        else:
+            Contact.id += 1
+            self.id = Contact.id
+            self.contact_list.append(self)
+
+    @property
+    def full_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+    @classmethod
+    def delete_contact(cls, id):
+        for contact in cls.contact_list:
+            if contact.id == id:
+                cls.contact_list.remove(contact)
+                return True
+            else: return False
+
+    @classmethod
+    def print_contacts(cls):
+        for contact in cls.contact_list:
+            print(contact.__dict__)
+            
+
+    @classmethod
+    def clear_all(cls):
+        if(len(cls.contact_list) == 0):
+                return "Empty"
+        else:
+            for contact in cls.contact_list:
+                cls.contact_list.remove(contact)
+
+    @classmethod
+    def find_contact_by_id(cls, id):
+        for contact in cls.contact_list:
+            if contact.id == id:
+                return contact.first_name
+
+    @classmethod
+    def find_all_contacts(cls):
+        return cls.contact_list
+            
+            
+
+    
--- Contacts_Test.py
+++ Contacts_Test.py
@@ -6,6 +6,37 @@ class ContactTest(unittest.TestCase):
         person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
         self.assertTrue(isinstance(person_x, Contact))
 
+    def test_save_contact(self):
+        Contact.clear_all()
+        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
+        person_x.save()
+        self.assertEqual(len(Contact.contact_list),1)
+        self.assertEqual(Contact.contact_list[0].first_name, person_x.first_name)
+
+    def test_delete_contact(self):
+        Contact.clear_all()
+        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
+        person_x.save()
+        self.assertEqual(Contact.delete_contact(1), True)
+
+    def test_find_contact_by_id(self):
+        Contact.clear_all()
+        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
+        person_x.save()
+        self.assertEqual(person_x.first_name,  Contact.find_contact_by_id(1))
+
+    def test_fullname(self):
+        person_x = Contact("Allan", "Walker", "076563738", "allanwarker@gmail.com")
+        self.assertEqual(person_x.full_name,"Allan Walker")
+
+    def test_find_all_contacts(self):
+        self.assertEqual(len(Contact.contact_list), len(Contact.find_all_contacts()))
+
+
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
